0b0b999aade4c642a1161ef6c204f16c198d60da
946493655 <946493655@qq.com>
2016-02-19 20:12:09 +0800

    小图标

--- resources/views/member/main.blade.php
+++ resources/views/member/main.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>微文化</title>
     <meta charset="utf-8">
+    <link rel="icon" type="image/png" href="assets/images/icon.png">
     <link rel="stylesheet" type="text/css" href="assets-home/css/home.css">
     <link rel="stylesheet" type="text/css" href="assets-home/css/home_body.css">
     <link rel="stylesheet" type="text/css" href="assets-home/css/creation.css">
