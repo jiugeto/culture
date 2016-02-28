7df59407dc9885aa77c9de5da9e70242dcb93989
root <root@localhost.(none)>
2016-02-29 05:06:49 +0800

    会员后台样式更新 2016-03-05

--- resources/views/member/home/index.blade.php
+++ resources/views/member/home/index.blade.php
@@ -1,4 +1,8 @@
 @extends('member.main')
 @section('content')
-    <div class="">窗口</div>
+    <div class="mem_float">
+        <div class="mem_right">
+            <div class="mem_win">窗口</div>
+        </div>
+    </div>
 @stop
--- resources/views/member/partials/menu.blade.php
+++ resources/views/member/partials/menu.blade.php
@@ -1,9 +1,12 @@
 {{-- 会员后台左侧菜单列表模板 --}}
 
-
-<div class="mem_left">
-    <ul class="mem_leftmenus">
-        <a href="#"><li class="a_li"><img src="#"> 首页</li></a>
-        <a href="#"><li class="a_li_curr"><img src="#"> 个人用户</li></a>
-    </ul>
+<div class="mem_float">
+    <div class="mem_left">
+        <ul class="mem_leftmenus">
+            <a href="#"><li class="a_li"><img src="#"> 首页</li></a>
+            <a href="#"><li class="a_li_curr"><img src="#"> 个人用户</li></a>
+            <a href="#"><li class="a_li"><img src="#"> 企业用户</li></a>
+            <a href="#"><li class="a_li"><img src="#"> 用户</li></a>
+        </ul>
+    </div>
 </div>
