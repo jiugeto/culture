54c715ceb2ef5ee78f9d27de10c69e602f0392e9
root <root@localhost.(none)>
2016-02-29 01:57:42 +0800

    navigate菜单栏修改

--- resources/views/layout/navigate.blade.php
+++ resources/views/layout/navigate.blade.php
@@ -3,20 +3,25 @@
 
 <!-- navigate菜单导航栏 -->
 <div class="nav">
-    <!-- navigate白底 -->
-    <div class="baidi">&nbsp;</div>
     <hr class="hr_style">
     <div class="nav_body">
-        <img src="assets-home/images/logo.png" class="logo">
-        <span class="nav_qiehuan"><a href="">导航</a></span>
-        <form action="">
-        <span class="nav_search">
-          <input type="text" class="global_search" name="global_search" placeholder="&nbsp;&nbsp;&nbsp;想要啥效果，赶紧找哦">
-          <input type="submit" class="global_search_text" value="搜 索">
-          <br><span class="key_word">搜索关键词：</span>
-        </span>
+        <div><img src="/assets-home/images/logo.png" class="logo"></div>
+        <div class="nav_qiehuan">
+            <a href=""><img src="/assets/images/daohang.png" style="width:15px;"> 导航</a>
+        </div>
+        <form action="" class="search">
+            <input type="text" class="global_search" name="global_search" placeholder="&nbsp;&nbsp;&nbsp;想要啥效果，赶紧找哦">
+            <input type="submit" class="global_search_text" value="搜 索">
         </form>
-        <div class="navigate">
+        <div class="key_word">搜索关键词：</div>
+        <div class="nav_right">
+            <a href=""><img src="#" style="width:15px;"> 登陆/注册</a>
+            <a href=""><img src="#" style="width:15px;"> 消息</a>
+            <a href=""><img src="#" style="width:15px;"> 记录</a>
+        </div>
+    </div>
+    <div class="navigate">
+        <div class="navigate_a">
             <a href="" class="curr">首&nbsp;页</a> /
             <a href="" class="nav_a">产品样片</a> /
             <a href="creation" class="nav_a">创作窗口</a> /
