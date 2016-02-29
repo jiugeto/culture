0ab81060a33967a6cac711640b4a5f83dbba2fc6
root <root@localhost.(none)>
2016-02-29 12:50:32 +0800

    注册样式更新 2016-03-06

--- resources/views/loginOrRegist/regist/index.blade.php
+++ resources/views/loginOrRegist/regist/index.blade.php
@@ -15,10 +15,10 @@
                         <tr><td>
                             <form action="/regist" method="POST" data-am-validator>
                                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                <div><input type="text" class="login_first" placeholder="&nbsp;&nbsp;&nbsp;用户名" name="username" required></div>
-                                <div><input type="text" placeholder="&nbsp;&nbsp;&nbsp;邮箱" name="email" required></div>
-                                <div><input type="text" placeholder="&nbsp;&nbsp;&nbsp;密码" name="password" required></div>
-                                <div><input type="text" class="login_last" placeholder="&nbsp;&nbsp;&nbsp;验证码" name="captcha" required></div>
+                                <div class="login_div login_first"><input type="text" placeholder="用户名(至少2位)" minlength="2" minlength="50" name="username" required></div>
+                                <div class="login_div"><input type="text" placeholder="邮箱" pattern="^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+\.[a-zA-Z0-9_-]{2,3}$" name="email" required></div>
+                                <div class="login_div"><input type="text" placeholder="密码(至少6位)" name="password" required></div>
+                                <div class="login_div login_last"><input type="text" placeholder="验证码" name="captcha" required><a href=""></a></div>
                                 <div>&nbsp;</div>
                                 <div><input type="submit" class="regist" value="注册会员"></div>
                             </form>
