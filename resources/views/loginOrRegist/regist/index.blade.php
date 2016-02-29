1da8b9c533fb6151d74aeed54910356788c966f2
root <root@localhost.(none)>
2016-02-29 16:35:54 +0800

    更新注册 2016-03-06

--- resources/views/loginOrRegist/regist/index.blade.php
+++ resources/views/loginOrRegist/regist/index.blade.php
@@ -16,8 +16,8 @@
                             <form action="/regist" method="POST" data-am-validator>
                                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                 <div class="login_div login_first"><input type="text" placeholder="用户名(至少2位)" minlength="2" minlength="50" name="username" required></div>
-                                <div class="login_div"><input type="text" placeholder="邮箱" pattern="^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+\.[a-zA-Z0-9_-]{2,3}$" name="email" required></div>
-                                <div class="login_div"><input type="text" placeholder="密码(至少6位)" name="password" required></div>
+                                <div class="login_div"><input type="text" placeholder="邮箱" pattern="^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$" name="email" required></div>
+                                <div class="login_div"><input type="text" placeholder="密码(至少6位)" minlength="6" name="password" required></div>
                                 <div class="login_div login_last"><input type="text" placeholder="验证码" minlength="4" maxlength="4" name="captcha" required><a href="" class="captcha">{!! captcha_img() !!}</a></div>
                                 <div>&nbsp;</div>
                                 <div><input type="submit" class="regist" value="注册会员"></div>
