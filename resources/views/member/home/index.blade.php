5f92c5f039a36a6c2ed39d6876d0fc45bf4aa362
946493655 <946493655@qq.com>
2016-03-07 18:59:42 +0800

    会员后台样式更新 2016-03-10

--- resources/views/member/home/index.blade.php
+++ resources/views/member/home/index.blade.php
@@ -1,14 +1,23 @@
 @extends('member.main')
 @section('content')
-    <div class="crumb">
-        <span>会员后台</span> /
-        <span>账户首页</span>
-    </div>
+    <div class="mem_crumb">会员后台 / 账户首页</div>
     <div class="mem_chart">
-        <div class="chart1">邮件消息</div>
-        <div class="chart1">公司</div>
-        <div class="chart1">订单</div>
-        <div class="mem_info">会员信息</div>
+        <div class="chart1">
+            <div class="circle circle1"></div>
+            <div class="chart_text">邮件消息</div>
+        </div>
+        <div class="chart1">
+            <div class="circle circle2"></div>
+            <div class="chart_text">公司</div>
+        </div>
+        <div class="chart1">
+            <div class="circle circle3"></div>
+            <div class="chart_text">订单</div>
+        </div>
+        <div class="mem_info">
+            <div class="circle"><img src="/assets/images/person.png" style="width:50px;"></div>
+            <div class="mem_user">MEMBER INFO<br>会员信息</div>
+        </div>
     </div>
-    <div class=""></div>
+    <div class="company_info"><button class="companybtn">公司信息</button></div>
 @stop
