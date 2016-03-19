eb52b7f5ead64d6b58d80e15f450e2babdde1f80
946493655 <946493655@qq.com>
2016-03-19 16:05:15 +0800

    更新数据库 2016-03-19

--- app/Http/Controllers/Member/HomeController.php
+++ app/Http/Controllers/Member/HomeController.php
@@ -7,9 +7,16 @@ class HomeController extends BaseController
      * 会员后台首页
      */
 
+    public function __construct()
+    {
+        $this->list['func']['name'] = '账户首页';
+        $this->list['func']['url'] = '';
+    }
+
     public function index()
     {
         $result = [
+            'menus'=> $this->list,
         ];
         return view('member.home.index', $result);
     }
