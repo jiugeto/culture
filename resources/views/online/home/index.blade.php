66cf14501b4ab0ed3a279533eb3a11e72c52928a
root <root@localhost.(none)>
2016-04-07 23:14:15 +0800

    创作窗口样式更新(自己电脑) 2016-04-07

--- resources/views/online/home/index.blade.php
+++ resources/views/online/home/index.blade.php
@@ -2,8 +2,15 @@
 @section('content')
     {{--  在线创建窗口 --}}
     <div class="online_win">
-        <div class="online_menu">
-            <span></span>
+        {{--<div class="menu">菜单</div>--}}
+        <div class="animate"></div>
+        <div class="timeline">
+            <div class="start">▶</div>
+            <div class="stop">■</div>
+            <div class="time">
+                <div></div>
+            </div>
+            <div class="right"><img src="/assets-home/images/voice.png"></div>
         </div>
     </div>
 @stop
