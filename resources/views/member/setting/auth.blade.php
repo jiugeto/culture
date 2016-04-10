58424d195a0b5816e2f700f6d0158b7ac4f0ffc8
jiuge <946493655@qq.com>
2016-04-10 18:04:56 +0800

    会员后台审核页面(公司) 2016-04-10

--- resources/views/member/setting/auth.blade.php
+++ resources/views/member/setting/auth.blade.php
@@ -6,40 +6,40 @@
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <input type="hidden" name="_method" value="POST">
 
-        <table class="table_create">
+        <table class="table_create" style="overflow:auto;">
             {{--基本信息--}}
             <tr><td colspan="2">
-                    <p class="center"><b>基本设置</b></p>
+                    <p class="center"><b>基本设置</b>(带<span class="star">*</span>的是必填项)</p>
                 </td></tr>
             <tr>
-                <td style="width:40%;"><label>用户名 / User Name：</label></td>
+                <td style="width:40%;"><label>用户名：</label></td>
                 <td>{{ $data->username }}
                     {{--<input type="text" placeholder="至少2个字符" minlength="2" required name="username" value="{{ $data->username }}"/>--}}
                 </td>
             </tr>
             <tr><td>&nbsp;</td></tr>
             <tr>
-                <td><label>邮箱 / Email：</label></td>
+                <td><label>邮箱：</label></td>
                 <td><input type="text" placeholder="例：123@qq.com" pattern="^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$" name="email" value="{{ $data->email }}"/></td>
             </tr>
             <tr><td>&nbsp;</td></tr>
             <tr>
-                <td><label>qq / QQ：</label></td>
+                <td><label>qq：</label></td>
                 <td><input type="text" name="qq" value="{{ $data->qq }}"/></td>
             </tr>
             <tr><td>&nbsp;</td></tr>
             <tr>
-                <td><label>电话 / Tel：</label></td>
+                <td><label>电话：</label></td>
                 <td><input type="text" name="tel" value="{{ $data->tel }}"/></td>
             </tr>
             <tr><td>&nbsp;</td></tr>
             <tr>
-                <td><label>手机 / Mobile：</label></td>
+                <td><label>手机：</label></td>
                 <td><input type="text" name="mobile" value="{{ $data->mobile }}"/></td>
             </tr>
             <tr><td>&nbsp;</td></tr>
             <tr>
-                <td><label>用户类型 / Genre：</label></td>
+                <td><label>用户类型<span class="star">(*)</span>：</label></td>
                 <td>
                     @foreach($isusers as $key=>$isuser)
                         <label><input type="radio" name="isuser" value="{{$key}}">{{ $isuser }}</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -50,19 +50,57 @@
             <tr><td>&nbsp;</td></tr>
 
             {{--个人信息--}}
-            <tr><td colspan="2">
-                    <p class="center"><b>个人信息</b></p>
-                </td></tr>
-            <tr>
-                <td><label>qq / QQ：</label></td>
-                <td><input type="text" name="qq" value="{{ $data->qq }}"/></td>
-            </tr>
-            <tr><td>&nbsp;</td></tr>
+            {{--<tr><td colspan="2">--}}
+                    {{--<p class="center"><b>个人信息</b></p>--}}
+                {{--</td></tr>--}}
+            {{--<tr>--}}
+                {{--<td><label>真实名字<span class="star">(*)</span>：</label></td>--}}
+                {{--<td><input type="text" name="qq" placeholder="至少5位" minlength="5"/></td>--}}
+            {{--</tr>--}}
+            {{--<tr><td>&nbsp;</td></tr>--}}
+            {{--<tr>--}}
+                {{--<td><label>性别<span class="star">(*)</span>：</label></td>--}}
+                {{--<td>--}}
+                    {{--<label><input type="radio" name="sex" value="1" checked/>男&nbsp;&nbsp;</label>--}}
+                    {{--<label><input type="radio" name="sex" value="2"/>女&nbsp;&nbsp;</label>--}}
+                {{--</td>--}}
+            {{--</tr>--}}
+            {{--<tr><td>&nbsp;</td></tr>--}}
+            {{--<tr>--}}
+                {{--<td><label>身份证<span class="star">(*)</span>：</label></td>--}}
+                {{--<td><input type="text" name="idcard" pattern="^\d{18}|(\d{17}x)$"/></td>--}}
+            {{--</tr>--}}
+            {{--<tr><td>&nbsp;</td></tr>--}}
+            {{--<tr>--}}
+                {{--<td><label>身份证正面照：</label></td>--}}
+                {{--<td><input type="text" name="infront"/></td>--}}
+            {{--</tr>--}}
+            {{--<tr><td>&nbsp;</td></tr>--}}
 
             {{--企业信息--}}
-            <tr><td colspan="2">
-                    <p class="center"><b>企业信息</b></p>
-                </td></tr>
+            {{--<tr><td colspan="2">--}}
+                    {{--<p class="center"><b>企业信息</b></p>--}}
+                {{--</td></tr>--}}
+            {{--<tr>--}}
+                {{--<td><label>公司名称：</label></td>--}}
+                {{--<td><input type="text" name="name" placeholder="至少2个字符" minlength="2"/></td>--}}
+            {{--</tr>--}}
+            {{--<tr><td>&nbsp;</td></tr>--}}
+            {{--<tr>--}}
+                {{--<td><label>地区：</label></td>--}}
+                {{--<td><input type="text" name="area"/></td>--}}
+            {{--</tr>--}}
+            {{--<tr><td>&nbsp;</td></tr>--}}
+            {{--<tr>--}}
+                {{--<td><label>具体地址：</label></td>--}}
+                {{--<td><input type="text" name="address" placeholder="至少2位字符" minlength="2"/></td>--}}
+            {{--</tr>--}}
+            {{--<tr><td>&nbsp;</td></tr>--}}
+            {{--<tr>--}}
+                {{--<td><label>营业执照号码：</label></td>--}}
+                {{--<td><input type="text" name="yyzzid"/></td>--}}
+            {{--</tr>--}}
+            {{--<tr><td>&nbsp;</td></tr>--}}
 
             {{--更改密码--}}
             <tr><td colspan="2"><div class="div_hr"></div></td></tr>
