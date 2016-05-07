9f21109c2e2ae6143ed23021f7ba2d174be03e71
jiuge <946493655@qq.com>
2016-05-07 18:30:37 +0800

    在线动画琢磨(公司) 2016-05-07

--- app/Http/routes/routes_member.php
+++ app/Http/routes/routes_member.php
@@ -22,7 +22,8 @@ Route::group(['prefix'=>'regist'], function(){
 /**
  * 在线创作路由
  */
-Route::group(['prefix'=>'online','middleware' =>'MemberAuth','namespace'=>'Online'],function(){
+//Route::group(['prefix'=>'online','middleware' =>'MemberAuth','namespace'=>'Online'],function(){
+Route::group(['prefix'=>'online','namespace'=>'Online'],function(){
     //主窗口
     Route::get('/','HomeController@index');
     Route::get('home','HomeController@index');
--- resources/views/online/home/index.blade.php
+++ resources/views/online/home/index.blade.php
@@ -1,76 +1,165 @@
 @extends('online.main')
 @section('content')
+    {{--内页的样式方便调节动画--}}
     <style>
-        .img div {
-            margin:0 auto;
-            padding-top:10px;
-            width:200px;
-            height:150px;
-            overflow:hidden;
-        }
-        .img .title { text-align:center; }
+        .img1 div { margin:0 auto; padding-top:10px; width:960px; height:540px; overflow:hidden; }
+        .img1 img { height:540px;/*border:2px solid white;*/ }
+        /*================================*/
+        .img2 { margin:0 auto; width:960px; height:540px; position:relative; top:-540px; }
+        .img2 div.pos { padding-left:5px; width:230px; height:520px; float:left; overflow:hidden; }
+        .img2 div.pos div { width:230px; height:520px; overflow:hidden; }
+        .img2 div.pos img { height:520px; }
+        /*================================*/
+        .img3 { margin:0 auto; width:960px; height:540px; position:relative; top:-1080px; }
+        .img3 div.pos { padding-left:5px; width:470px; height:520px; float:left; overflow:hidden; }
+        .img3 div.pos div { width:470px; height:520px; overflow:hidden; }
+        .img3 div.pos img { height:520px; }
+        /*================================*/
+        .img4 { margin:0 auto; width:960px; height:540px; position:relative; top:-2720px; }
+        .img4 div.pos { padding:5px; width:950px; height:530px; }
+        .img4 div.pos div { width:950px; height:530px; overflow:hidden; }
+        .img4 div.pos img { height:540px; }
+        /*================================*/
+        .img5 { margin:0 auto; width:1920px; height:540px; position:relative; top:-3260px; }
+        .img5 div.pos { width:1920px; height:540px; }
+        .img5 div.pos div { width:960px; height:540px; float:left; overflow:hidden; }
+        .img5 div.pos img { height:540px; }
+
+        {{--动画样式--}}
         /*动画的代码开始：定义动画时间*/
-        .img {
-            position:relative;
-            animation-name:myfirst;
-            animation-duration:5s;
-            animation-timing-function:linear;
-            animation-delay:2s;
-            animation-iteration-count:infinite;
-            animation-direction:alternate;
-            animation-play-state:running;
+        .img1 {
+            position:relative; top:540px;
+            opacity:1;
+            animation-name:animate1;
+            animation-duration:1.5s;
+            animation-timing-function:ease;
+            animation-delay:0s;
+            /*animation-iteration-count:infinite;*/
+            /*animation-direction:alternate;*/
+            /*animation-play-state:running;*/
+            animation-fill-mode:forwards;
         }
+        .img2 .pos div {
+            position:relative; left:500px; opacity:0;
+            animation-name:animate2;
+            animation-duration:3s;
+            animation-timing-function:ease;
+            animation-delay:1s;
+            animation-fill-mode:forwards;
+        }
+        .img3 .pos div {
+            position:relative; left:250px; opacity:0;
+            animation-name:animate3;
+            animation-duration:3s;
+            animation-timing-function:ease;
+            animation-delay:3s;
+            animation-fill-mode:forwards;
+        }
+        .img4 .pos div {
+            position:relative; top:0px;
+            animation-name:animate4;
+            animation-duration:6.5s;
+            animation-timing-function:ease;
+            animation-delay:5.5s;
+            animation-fill-mode:forwards;
+        }
+        .img5 .pos div {
+            position:relative; top:0px;
+            animation-name:animate5;
+            animation-duration:6s;
+            animation-timing-function:ease;
+            animation-delay:10s;
+            animation-fill-mode:forwards;
+        }
+
         /*动画的代码开始*/
-        @keyframes myfirst
+        @keyframes animate1
         {
-            /*from {background:red;}*/
-            /*to {background:yellow;}*/
-            0%  {left:0px; top:0px;}
-            50%  {left:200px; top:0px;}
-            100%  {left:0px; top:200px;}
+            /*from { top:600px; }*/
+            /*to { top:-10px; }*/
+            0% { top:600px; }
+            20% { top:-10px; }
+            70% { top:-10px; left:0px; opacity:1; }
+            100% { top:-10px; left:-200px; opacity:0; }
         }
-        @-moz-keyframes myfirst /* Firefox */
+        @keyframes animate2
         {
-            /*from {background:red;}*/
-            /*to {background:yellow;}*/
-            0%  {left:0px; top:0px;}
-            100%  {left:100px; top:0px;}
+            0% { left:250px; opacity:1; }
+            40% { left:5px; opacity:1; }
+            70% { left:5px; opacity:1; }
+            100% { left:-500px; opacity:0; }
         }
-        @-webkit-keyframes myfirst /* Safari and Chrome */
+        @keyframes animate3
         {
-            /*from {background:red;}*/
-            /*to {background:yellow;}*/
-            0%  {left:0px; top:0px;}
-            100%  {left:100px; top:0px;}
+            0% { left:500px; }
+            25% { left:5px; opacity:1; }
+            80% { left:5px; top:0px; opacity:1; }
+            100% { left:5px; top:540px; opacity:0; }
         }
-        @-o-keyframes myfirst /* Opera */
+        @keyframes animate4
         {
-            /*from {background:red;}*/
-            /*to {background:yellow;}*/
-            0%  {left:0px; top:0px;}
-            100%  {left:100px; top:0px;}
+            0% { top:0px; }
+            10% { top:550px; }
+            40% { top:550px; }
+            50% { top:1090px; }
+            90% { top:1090px; opacity:1; }
+            100% { top:1640px; opacity:0; }
+        }
+        @keyframes animate5
+        {
+            0% { top:0px; }
+            10% { top:550px; }
+            40% { top:550px; left:0px; }
+            50% { top:550px; left:-960px; }
+            90% { top:550px; left:-960px; opacity:1; }
+            100% { top:550px; left:-960px; opacity:0; }
         }
     </style>
 
-    {{-- link：/assets-home/css/online.css --}}
     {{--  在线创建窗口 --}}
     <div class="online_win">
-        {{--<div class="menu">菜单</div>--}}
-        {{--动画模板--}}
-        <div class="animate">
-            <div class="img">
+        <div class="animate" style="background:white;">
+            <div class="img1">
                 <div><img src="/uploads/images/2016/ppt.png"></div>
-                <div class="title">图片1</div>
             </div>
-        </div>
-        {{--时间线--}}
-        <div class="timeline">
-            <div class="start">▶</div>
-            <div class="stop">■</div>
-            <div class="time">
-                <div></div>
+            <div class="img2">
+                <div class="pos" style="padding-left:10px;">
+                    <div><img src="/uploads/images/2016/online1.png"></div>
+                </div>
+                <div class="pos">
+                    <div><img src="/uploads/images/2016/online1.png"></div>
+                </div>
+                <div class="pos">
+                    <div><img src="/uploads/images/2016/online1.png"></div>
+                </div>
+                <div class="pos">
+                    <div><img src="/uploads/images/2016/online1.png"></div>
+                </div>
             </div>
-            <div class="right"><img src="/assets-home/images/voice.png"></div>
+            <div class="img3">
+                <div class="pos">
+                    <div><img src="/uploads/images/2016/online1.png"></div>
+                </div>
+                <div class="pos">
+                    <div><img src="/uploads/images/2016/online1.png"></div>
+                </div>
+            </div>
+            <div class="img4">
+                <div class="pos">
+                    <div><img src="/uploads/images/2016/ppt.png"></div>
+                </div>
+            </div>
+            <div class="img4">
+                <div class="pos">
+                    <div><img src="/uploads/images/2016/ppt.png"></div>
+                </div>
+            </div>
+            <div class="img5">
+                <div class="pos">
+                    <div><img src="/uploads/images/2016/ppt.png"></div>
+                    <div><img src="/uploads/images/2016/ppt.png"></div>
+                </div>
+            </div>
         </div>
     </div>
 @stop
